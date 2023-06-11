9634a4818bbf19041d977d8eda0a4cacafbd8506
leehyeonseop <gustjq4146@naver.com>
2023-06-11 22:13:24 +0900

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+
+    console.log('hello')
+    return (
+        <div>
+            <div>@2023 Lamamia. All rights reserved.</div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Footer
